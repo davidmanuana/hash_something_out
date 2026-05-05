08689c10710eebe8c4c12a5880c66437eab2bb91
Daviddayoni <daviddayonipro@gmail.com>
2026-05-05 18:03:47 -0400

    Fixed duplicate key overwrite issue in linked list and linear probing hash tables

--- hash_something_out.py
+++ hash_something_out.py
@@ -7,6 +7,7 @@
 import time
 import csv
 
+
 class MovieRecord:
     # store one movie row
     def __init__(self, title, quote):
@@ -15,6 +16,7 @@ class MovieRecord:
 
     def __str__(self):
         return self.title + " | " + self.quote
+
 
 class Node:
     # linked list node
@@ -26,16 +28,32 @@ class Node:
 
 class LinkedHashTable:
     # linked list chaining
-    def __init__(self, size):
+    def __init__(self, size, hash_type):
         self.size = size
+        self.hash_type = hash_type
         self.table = [None] * size
         self.collisions = 0
         self.items = 0
 
-    # ATTEMPT 1 HASH
+    # choose hash function
     def hash_function(self, key):
-        return len(key) % self.size
+        if self.hash_type == 1:
+            return len(key) % self.size
 
+        if self.hash_type == 2:
+            total = 0
+            i = 0
+            for ch in key:
+                total += ord(ch) * (i + 1)
+                i += 1
+            return total % self.size
+
+        total = 0
+        for ch in key:
+            total = (total * 31 + ord(ch)) % self.size
+        return total
+
+    # add item, do not overwrite duplicates
     def add(self, key, data):
         index = self.hash_function(key)
         new_node = Node(key, data)
@@ -46,18 +64,16 @@ class LinkedHashTable:
             return
 
         cur = self.table[index]
-        while cur != None:
+
+        while cur.next != None:
             self.collisions += 1
-            if cur.key == key:
-                cur.data = data
-                return
-            if cur.next == None:
-                break
             cur = cur.next
 
+        self.collisions += 1
         cur.next = new_node
         self.items += 1
 
+    # search for first matching key
     def search(self, key):
         index = self.hash_function(key)
         cur = self.table[index]
@@ -66,47 +82,65 @@ class LinkedHashTable:
             if cur.key == key:
                 return cur.data
             cur = cur.next
+
         return None
 
+    # count empty buckets
     def wasted_space(self):
         count = 0
+
         for bucket in self.table:
             if bucket == None:
                 count += 1
+
         return count
 
 
 class LinearHashTable:
     # linear probing
-    def __init__(self, size):
+    def __init__(self, size, hash_type):
         self.size = size
+        self.hash_type = hash_type
         self.table = [None] * size
         self.collisions = 0
         self.items = 0
 
-    # ATTEMPT 3/4/5 HASH
+    # choose hash function
     def hash_function(self, key):
+        if self.hash_type == 3:
+            if key == "":
+                return 0
+            return (ord(key[0]) + len(key)) % self.size
+
+        if self.hash_type == 4:
+            total = 0
+            i = 0
+            for ch in key:
+                total += ord(ch) * (i + 3)
+                i += 1
+            return total % self.size
+
         total = 5381
         for ch in key:
             total = ((total * 33) + ord(ch)) % self.size
         return total
-    
+
+    # add item, do not overwrite duplicates
     def add(self, key, data):
         index = self.hash_function(key)
         start = index
 
         while self.table[index] != None:
             self.collisions += 1
-            if self.table[index][0] == key:
-                self.table[index] = (key, data)
-                return
             index = (index + 1) % self.size
+
             if index == start:
                 return
 
         self.table[index] = (key, data)
         self.items += 1
 
+    # search for first matching key
     def search(self, key):
         index = self.hash_function(key)
         start = index
@@ -114,17 +148,24 @@ class LinearHashTable:
         while self.table[index] != None:
             if self.table[index][0] == key:
                 return self.table[index][1]
+
             index = (index + 1) % self.size
+
             if index == start:
                 break
+
         return None
 
+    # count empty slots
     def wasted_space(self):
         count = 0
+
         for slot in self.table:
             if slot == None:
                 count += 1
+
         return count
+
 
 def load_movies(filename):
     movies = []
@@ -144,6 +185,7 @@ def load_movies(filename):
     f.close()
     return movies
 
+
 def build_table(table, movies, use_title):
     start = time.time()
 
@@ -152,10 +194,12 @@ def build_table(table, movies, use_title):
             key = movie.title
         else:
             key = movie.quote
+
         table.add(key, movie)
 
     end = time.time()
     return end - start
+
 
 def print_stats(name, method_name, table, build_time):
     print(name)
@@ -167,23 +211,74 @@ def print_stats(name, method_name, table, build_time):
     print("Construction time:", build_time)
     print()
 
+
+def run_attempt(attempt_name, table_kind, size, hash_type, movies):
+    print("==============================")
+    print(attempt_name)
+    print("==============================")
+    print()
+
+    if table_kind == "linked":
+        title_table = LinkedHashTable(size, hash_type)
+        quote_table = LinkedHashTable(size, hash_type)
+        method = "Linked List"
+    else:
+        title_table = LinearHashTable(size, hash_type)
+        quote_table = LinearHashTable(size, hash_type)
+        method = "Linear Probing"
+
+    title_time = build_table(title_table, movies, True)
+    quote_time = build_table(quote_table, movies, False)
+
+    print_stats("Hash Table 1: Movie Title as Key", method, title_table, title_time)
+    print_stats("Hash Table 2: Movie Quote as Key", method, quote_table, quote_time)
+
+
 def main():
     movies = load_movies("MOCK_DATA.csv")
 
     print("Total movie records loaded:", len(movies))
     print()
 
- # ATTEMPTS
-    title_table = LinkedHashTable(17011)
-    quote_table = LinkedHashTable(17011)
+    run_attempt(
+        "ATTEMPT 1 - LINKED LIST / POOR HASH",
+        "linked",
+        1009,
+        1,
+        movies
+    )
 
-    t1 = build_table(title_table, movies, True)
-    t2 = build_table(quote_table, movies, False)
+    run_attempt(
+        "ATTEMPT 2 - LINKED LIST / BETTER HASH",
+        "linked",
+        2003,
+        2,
+        movies
+    )
 
-    print("ATTEMPT 5 - LINEAR PROBING / ROLLING HASH")
-    print()
-    print_stats("Hash Table 1: Movie Title as Key", "Linked List", title_table, t1)
-    print_stats("Hash Table 2: Movie Quote as Key", "Linked List", quote_table, t2)
+    run_attempt(
+        "ATTEMPT 3 - LINEAR PROBING / POOR HASH",
+        "linear",
+        20011,
+        3,
+        movies
+    )
+
+    run_attempt(
+        "ATTEMPT 4 - LINEAR PROBING / BETTER HASH",
+        "linear",
+        20011,
+        4,
+        movies
+    )
+
+    run_attempt(
+        "ATTEMPT 5 - LINEAR PROBING / ROLLING HASH",
+        "linear",
+        17011,
+        5,
+        movies
+    )
 
 
 if __name__ == "__main__":
